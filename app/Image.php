da108a214314b142575128b5fd1d4e14bd207266
anunde <a.nunez.torre@gmail.com>
2020-03-08 22:55:44 +0100

    Inicio el proyecto y habilito el registro de usuarios

---
+++ app/Image.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $table = 'images';
+
+    //Relación One To Many / de uno a muchos
+    public function comments() {
+    	return $this->hasMany('App\Comment');
+    }
+
+    //Relación One To Many
+    public function likes() {
+    	return $this->hasMany('App\Like');
+    }
+
+    //Relación Many To One
+    public function user(){
+    	return $this->belongsTo('App\User', 'user_id');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//use App\Image;
+
+Route::get('/', function () {
+	/*	
+	$images = Image::all();
+	foreach ($images as $image) {
+	echo $image->image_path."<br>";
+	echo $image->description."<br>";
+	echo $image->user->name." ".$image->user->surname."<br>";
+
+	if (count($image->comments) >= 1) {
+		echo '<h4>Comentarios</h4>';
+		foreach ($image->comments as $comment) {
+		echo "<strong>".$comment->user->nick." : </strong>".$comment->content."<br>";
+		}
+
+	}
+
+	echo 'Likes: '.count($image->likes);
+	echo "<hr>";
+	}
+	die();
+	*/
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
